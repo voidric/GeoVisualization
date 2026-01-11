0c30677e5fce0c6ff5d85a134d36c87c21214414
voidric <lfzm66@gmail.com>
2026-01-11 22:52:35 +0800

    Remove new_dem.tif

--- GeoVisSystem/vite.config.js
+++ GeoVisSystem/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
     base: './', // 确保构建后使用相对路径，方便部署
+    plugins: [viteSingleFile()],
     server: {
         open: false,
         host: true
